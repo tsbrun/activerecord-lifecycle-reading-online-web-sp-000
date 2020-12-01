f5f229b8e7907203c9f8c691315d9d67eed0d322
TSephanya Brun <tsbrun@icloud.com>
2020-12-01 16:24:22 -0500

    commit tree eead2f7f5289f754420e126f6df6864cfc2fb93c

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  # before_save :make_title_case => didn't work because object must be valid before it saves; if not valid, it never gets to before_save
+
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -13,5 +19,9 @@ class Post < ActiveRecord::Base
 
   def make_title_case
     self.title = self.title.titlecase
+  end
+
+  def email_author_about_post
+    
   end
 end
